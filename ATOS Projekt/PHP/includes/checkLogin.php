5ed0589e231abfc55a07c77e819217ac9d4d971b
Simon Wosnitza <95503436+WoSimon@users.noreply.github.com>
2022-06-14 23:05:06 +0200

    Import der Excel Datei mit den Patienten möglich gemacht

--- ATOS Projekt/PHP/includes/checkLogin.php
+++ ATOS Projekt/PHP/includes/checkLogin.php
@@ -1,12 +1,27 @@
 <?php 
 
-    if (isset($_POST['nutzer'])){
+    require '../../vendor/autoload.php';
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+    /*$spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setCellValue('A1', 'Hello World !');
+
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('hello world.xlsx');*/
+
+    if (isset($_POST['nutzer'])){
         
+        $reader = new PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $spreadsheet = $reader->load('../../Input/Patienten.xlsx');
+        $sheetdata = $spreadsheet->getActiveSheet()->toArray();
+        echo "<pre>";
+        print_r($sheetdata);
 
     }
     else{
-        header("Location: ../HTML/login.php?error=1");
+        header("Location: ../../HTML/login.php?error=1");
     }
 
 ?>
